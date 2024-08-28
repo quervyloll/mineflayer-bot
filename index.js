fee3059e92e112c7dd65344c0aff9ed482d89ba3
quervylol <121582098+quervyloll@users.noreply.github.com>
2024-08-27 20:44:29 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const mineflayer = require('mineflayer')
 const { username, password, server, version, auth, port, prefix} = require('./config.json')
 
 
-function createBot() {
+function createBot() { // Function to create the Minecraft bot, that way so if your bot is disconnected, it will autoreconnect.
     const bot = mineflayer.createBot({
     host: server,
     auth: auth, 
@@ -17,21 +17,21 @@ function createBot() {
 bot.on('error', (err) => console.log(err))
 bot.on('kicked', console.log)
 bot.on('end', () => {
-    setTimeout(createBot, 5000)
+    setTimeout(createBot, 5000) // Reconnect to the server, default is set to 5 seconds (5,000ms).
 })
 
 bot.on('login', () => {
     console.log(`Bot is on ${server}!`)
 })
 
-bot.commands = new Map()
+bot.commands = new Map() // Initializes the bot's command storage.
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'))
 for (const file of commandFiles) {
     const command = require(`./commands/${file}`)
     bot.commands.set(command.name, command)
 }
 
-bot.on('chat', (username, message) => {
+bot.on('chat', (username, message) => { // A basic event handler for the bot to listen to chat messages and respond if a command is called.
     if (username === bot.username) return
     if (!message.startsWith(prefix)) return
 
