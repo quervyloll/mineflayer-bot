af0dd93d9b871597f695478ee8be37e779ce7a2c
quervylol <121582098+quervyloll@users.noreply.github.com>
2024-08-27 20:40:42 -0400

    Update example.js

--- commands/example.js
+++ commands/example.js
@@ -1,7 +1,7 @@
 module.exports = {
-    name: 'example',
-    description: 'I am an example command! Write all about me here!',
+    name: 'example', // The name that can be used in minecraft chat. Make sure you use the prefix!
+    description: 'I am an example command! Write all about me here!', // A description that can provide more insight to a command, doesn't have to be used. I personally like descriptions.
     execute(bot, username, args) {
-        bot.chat("Thanks for using my example bot!")
+        bot.chat("Thanks for using my example bot!") // The code for each command.
     }
 }
